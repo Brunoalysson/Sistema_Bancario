9a190bae029749f37b8baa837adaa072a36e5502
Bruno Alysson <bruno.alysson@hotmail.com>
2022-06-05 21:32:23 -0300

    Criação do Menu

--- conta.py
+++ conta.py
@@ -1,8 +1,10 @@
 class Conta:
+    
     def __init__(self, numero, saldo=0):
         self.numero = numero
         self.saldo = saldo
-    
+        print("Conta criada")
+        
     def setNumero(self, numero):
         self.numero = numero
 
@@ -14,10 +16,14 @@ class Conta:
     
     def getSaldo(self):
         return self.saldo
+        
+    def imprimeSaldo(self, numero):
+        self.numero = numero
+        print("Saldo: R$ %2.f" %self.saldo)
 
     def credito(self, numero, valor):
-        numero.saldo += valor
-        print("Novo Saldo:", numero.saldo)
+        self.saldo += valor
+        print("Novo Saldo:", self.saldo)
     
     def debito(self, numero, valor):
         numero.saldo -= valor
--- main.py
+++ main.py
@@ -1,15 +1,59 @@
 from conta import Conta
 
-print("Sistema Bancário")
+resposta = 'S'
 
-number = int(input("Digite o número da conta:"))
+contas=[]
 
-conta1 = Conta(number)
-print("Número da conta:", conta1.numero)
-print("Saldo:", conta1.saldo)
-
-credito = int(input("Digite o valor do crédito:"))
-credits = conta1.credito(conta1 ,credito)
-
-debito = int(input("Digite o valor do débito:"))
-debits = conta1.debito(conta1, debito)
+while (not resposta == 'N'):
+    print("\nMENU\n")
+    print("1- Criar conta")
+    print("2- Consultar saldo")
+    print("3- Crédito")
+    print("4- Débito")
+    print("5- Transferência")
+    
+    escolha = int(input("Digite a operação que deseja fazer:"))
+    
+    if escolha ==1:
+        number = int(input("Digite o número da conta:"))
+        nova = Conta(number)
+        '''contas.append(nova.numero)
+        contas.append(nova.saldo)'''
+        contas.append(nova)
+        
+    if escolha ==2:
+        number = int(input("Digite o número da conta:"))
+        for i in range(len(contas)):
+            if number == contas[i].numero:
+                print("Saldo:", contas[i].saldo)
+        
+    if escolha ==3:
+        number = int(input("Digite o número da conta:"))
+        valor = int(input("Digite o valor do crédito:"))
+        for i in range (len(contas)):
+            if number == contas[i].numero:
+                contas[i].saldo+= valor
+                print("Saldo:", contas[i].saldo)
+                
+    if escolha ==4:
+        number = int(input("Digite o número da conta:"))
+        valor = int(input("Digite o valor do dédito:"))
+        for i in range (len(contas)):
+            if number == contas[i].numero:
+                contas[i].saldo -= valor
+                print("Saldo:", contas[i].saldo)
+    
+    if escolha ==5:
+        contaOrigem = int(input("Digite o número da conta de origem:"))
+        contaDestino = int(input("Digite o número da conta de destino:"))
+        valor = int(input("Digite o valor da transferência:"))
+        for i in range(len(contas)):
+            if contaOrigem == contas[i].numero:
+                contas[i].saldo -= valor
+        for i in range(len(contas)):
+            if contaDestino == contas[i].numero:
+                contas[i].saldo += valor
+        print("Transferência Realizada")
+        
+    resposta = input("Deseja fazer outra operação? S/N:")
+    
